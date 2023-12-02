cb2ecb5d7b690abd62113e30da26161efa7b8889
Mauro Sebastian Ferraresi <msferraresi@gmail.com>
2023-12-02 02:32:50 -0300

    Se realiza modificacion sobre PetTypeService para ofrecer un GET que entregue todos los tipos de mascotas con las que se trabajan y cargar el combo

--- src/services/petTypeService.py
+++ src/services/petTypeService.py
@@ -8,3 +8,10 @@ app = Blueprint('pet_type',__name__,url_prefix='/pet_type')
 schema = PetTypeSchema()
 schemas = PetTypeSchema(many=True)
 
+@app.route('', methods=['GET'])
+def get_pet_types():
+    lst_units = PetType.query.order_by(PetType.id.asc()).filter_by(deleted_at=None).all()
+    if not lst_units:
+        return jsonify({'message': 'No pet types found'}), 404
+    else:
+        return jsonify({'pet_types':schema.dump(lst_units, many=True)}), 200
